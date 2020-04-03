64e869fc4486f111335b13f42fd815ff0c2c3fa4
root <root@PC.localdomain>
2020-04-03 15:39:50 -0300

    Adicionado os graficos para representacao

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
-/*const os = require('os');
-
-
-
-
-function myFunc() {
-    console.log("Memória : " + os.cpus().c);
-}
-  
-setInterval(myFunc, 1000);*/
-
 const os = require('os-utils');
 const disk = require('check-disk-space');
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
+const screen = blessed.screen();
+const grid = new contrib.grid({rows: 12, cols: 12, screen: screen});
+
+const donutHd = grid.set(0,0,6,6, contrib.donut ,{
+    label: 'HD',
+	radius: 16,
+	arcWidth: 3,
+	remainColor: 'black',
+	yPadding: 2,
+	data: [
+	  {percent: 100, label: 'C:', color: 'green'}
+	]
+});
+
+const gaugeMem = grid.set(0,6,6,6,contrib.gauge, {
+    label: 'Ram', 
+    stroke: 'green', 
+    fill: 'white'
+});
+gaugeMem.setPercent(100);
+let values=[5, 1, 7, 5, 8,12];
+
+const lineCpu =grid.set(6,0,6,12, contrib.line,
+    { style:
+      { line: "yellow"
+      , text: "green"
+      , baseline: "black"}
+    , xLabelPadding: 3
+    , xPadding: 5
+    , height: 15
+    , label: 'CPU'})
+, data = {
+    x: ['t1', 't2', 't3', 't4', 't5'],
+    y: [0, 0, 0, 0, 0, 0]
+ }
+
+ lineCpu.setData(data);
+
+
+
+screen.key(['escape'], function(ch, key) {
+    return process.exit(0);
+});
+
+//--------------------------------------
 
 const pcResources = {
     cpuUsage: 0,
-    cpuFree: 0,
     hdUsage: 0,
-    hdFree: 0,
     memUsage: 0,
-    memFree: 0,
 };
 
 
@@ -26,21 +59,33 @@ function resources(){
     os.cpuUsage(function(val){
         pcResources.cpuUsage=val*100;
     });
-    
-    os.cpuFree(function(val){
-        pcResources.cpuFree=val*100;
-    });
-
     disk("/").then(result => {
-        pcResources.hdUsage = (result.size - result.free)/1024/1024/1024;
-        pcResources.hdFree = result.free/1024/1024/1024;
+        pcResources.hdUsage = (result.size - result.free) * 100 / result.size;
     });
 
-    pcResources.memUsage = (os.totalmem() - os.freemem())/1024;
-
-    pcResources.memFree = os.freemem()/1024;
-
-    console.log(pcResources);
+    pcResources.memUsage = (os.totalmem() - os.freemem()) * 100 / os.totalmem() ;
+    
+    createGraphcs(pcResources);
 }
 
 setInterval(resources, 1000);
+
+
+function createGraphcs(x){
+    donutHd.setData([{percent: x.hdUsage, label: 'C:', color: 'green'}]);
+    gaugeMem.setPercent(x.memUsage);
+
+    for(i=6; i < 0;i++){
+        values[i ] = values[i - 1];
+    }
+    values[0] =values[1];
+    values[1] =values[2];
+    values[2] =values[3];
+    values[3] =values[4];
+    values[4] =values[5];
+    values[5] = x.cpuUsage;
+
+    lineCpu.setData({x: [' ', ' ', ' ', ' ', ' '], y: values});
+    
+    screen.render();
+}
